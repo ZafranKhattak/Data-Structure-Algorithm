e5e49b45c2dd86c1c2839bc95175c75ee3deb388
ZafranKhattak <zafrankhansoftware.engineer@gmail.com>
2026-09-08 18:33:18 +0500

    Updated Circular

--- Circular LinkedList/Add at Front/Main.java
+++ Circular LinkedList/Add at Front/Main.java
@@ -15,31 +15,74 @@ class CircularLinkedList {
         this.head = null;
     }
 
+    // ADD AT FRONT
     void addFront(int data) {
 
-    Node firstNode = new Node(data);
+        Node firstNode = new Node(data);
 
-    if (head == null) {
-        head = firstNode;
-        firstNode.next = head;
+        if (head == null) {
+            head = firstNode;
+            firstNode.next = head;
+        } else {
+            Node current = head;
+
+            // last node find karo
+            while (current.next != head) {
+                current = current.next;
+            }
+
+            firstNode.next = head;
+            current.next = firstNode;
+            head = firstNode;
+        }
     }
-    else {
+
+    // ADD AT BACK
+    void addBack(int data) {
+        Node addBack = new Node(data);
+        if (head == null) {
+            head = addBack;
+            addBack.next = head;
+            return;
+        }
+
+        else {
+            Node current = head;
+
+            while (current.next != head) {
+                current = current.next;
+            }
+
+            current.next = addBack;
+            addBack.next = head;
+        }
+    }
+
+    // DELETE FRONT
+    void deleteFront() {
+        if (head == null) {
+            System.out.print("List is Empty");
+            return;
+        }
+
+        if (head.next == head) {
+            head = null;
+            return;
+        }
+
+        head = head.next;
+
         Node current = head;
 
-        // last node find karo
         while (current.next != head) {
             current = current.next;
         }
 
-        firstNode.next = head;
-        current.next = firstNode;
-        head = firstNode;
+        current.next = head;
     }
-}
 
-    // DISPLAY METHOD
-
-    void display()
+    // DELETE BACK METHO
+    void deleteBack()
     {
         if(head == null)
         {
@@ -47,13 +90,33 @@ class CircularLinkedList {
             return ;
         }
 
-        Node current = head;       
-        do 
+        if (head.next == head)
         {
-            System.out.print(current.data + " ");
+            head = null;
+            return ;
+        }
+
+        Node current = head;
+
+        while(current.next.next!= head)
+        {
             current = current.next;
         }
-        while(current != head);
+
+        current.next.next = head;
+    }
+    // DISPLAY METHOD
+    void display() {
+        if (head == null) {
+            System.out.print("List is Empty");
+            return;
+        }
+
+        Node current = head;
+        do {
+            System.out.print(current.data + " ");
+            current = current.next;
+        } while (current != head);
     }
 }
 
@@ -62,8 +125,10 @@ public class Main {
 
         CircularLinkedList list = new CircularLinkedList();
         list.addFront(10);
-        list.addFront(10);
+        list.addFront(20);
 
+        list.deleteFront();
+        list.deleteBack();
         list.display();
     }
 }
