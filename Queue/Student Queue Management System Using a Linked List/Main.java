d05555e8c7d1fd2011d1c2aee10964b48dff7653
ZafranKhattak <zafrankhansoftware.engineer@gmail.com>
2026-09-15 22:40:56 +0500

    Update Queue

---
+++ Queue/Student Queue Management System Using a Linked List/Main.java
@@ -0,0 +1,32 @@
+class Student
+{
+    int studendId ;
+    String studentName;
+    int studentAge;
+    double studentCGPA;
+    double studentInterPercentage;
+
+    public Student(int studentId,
+                   String studentName,
+                   int studentAge,
+                   double studentCGPA,
+                   doubel studentInterPercentage
+                                                )
+
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.studentAge = studentAge;
+        this.studentCGPA = studentCGPA;
+        this.studentInterPercentage = studentInterPercentage;
+    }
+
+}
+
+//      ================ MAIN CLASS =================
+public class Main {
+    public static void main(String args[])
+    {
+    }
+}
+
+
